9859b230a29c52284521bd5e4079b755f9ad31b9
trevinkur <mokeyprojek@gmail.com>
2022-09-17 06:22:12 +0700

    fixing sign up

--- src/Layouts/Auth/SignUp.js
+++ src/Layouts/Auth/SignUp.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router';
@@ -7,24 +8,36 @@ const SignUp = () => {
     const [email, setEmail] = useState("");
     const [pwd, setPwd] = useState("");
     const [phoneNumber, setPhoneNumber] = useState("");
+    const [username, setUsername] = useState('')
+    const [errMsg, setErrMsg] = useState()
+    const [loading, setLoading] = useState(false)
     const router = useRouter();
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        fetch(`${process.env.URL_BE}api/v1/auth/register`,
-        {
+        
+        setLoading(true)
+        axios({
             method: "POST",
-            body: JSON.stringify({
-                email: email,
+            url: `${process.env.URL_BE}api/v1/auth/register`,
+            data: {
+                display_name: username,
+                email,
                 password: pwd,
                 phone_number: phoneNumber
-            }),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8"
             }
-        }).then(res => res.json())
-        .then(()=> {alert("succes");router.replace("/auth/signin")})
-        .catch(err => alert(`${err}`))
+        })
+        .then(res => {
+            alert('success')
+            router.replace("/auth/signin")
+        }).catch(err => {
+            if(err.response.status === 400) {
+                setErrMsg(err.response.data.message)
+            } else if(err.response.status === 500) {
+                setErrMsg('server is down')
+            }
+        })
+        .finally(() => setLoading(false))
     }
   return (
     <div className='col-md-6' style={{height: "100vh", overflowY: "auto" }}>
@@ -42,27 +55,53 @@ const SignUp = () => {
             <h2 className='color-secondary text-center'>Sign Up</h2>
         <form onSubmit={handleSubmit} className='p-5'>
             <div className="mb-3">
+            <label htmlFor="username" className="form-label">Username</label>
+                <input type="text" className="form-control form-control-lg"
+                 id="username" placeholder="your username"
+                 onChange={(e) => {setUsername(e.target.value); setErrMsg('')}}
+                 required />
+            </div>
+            <div className="mb-3">
             <label htmlFor="email" className="form-label">Email address</label>
                 <input type="email" className="form-control form-control-lg"
                  id="email" placeholder="name@example.com"
-                 onChange={(e) => setEmail(e.target.value)}
+                 onChange={(e) => {setEmail(e.target.value); setErrMsg('')}}
                  required />
             </div>
             <div className="mb-3">
             <label htmlFor="password" className="form-label">Password</label>
                 <input type="password" className="form-control form-control-lg" id="password" 
-                onChange={(e) => setPwd(e.target.value)} required/>
+                onChange={(e) => {setPwd(e.target.value); setErrMsg('')}} required/>
             </div>
             <div className="mb-3">
             <label htmlFor="phoneNumber" className="form-label">Phone Number</label>
                 <input type="tel" className="form-control form-control-lg" id="phoneNumber"
                  placeholder="083*******" 
-                 onChange={(e) => setPhoneNumber(e.target.value)}
+                 onChange={(e) => {setPhoneNumber(e.target.value); setErrMsg('')}}
+                 required  
                  />
             </div>
-            <div className='d-grid gap-2'>
-                <button className='btn btn-primary py-3' onClick={handleSubmit}>Sign Up</button>
-            </div>
+
+            {errMsg && <div
+                style={{backgroundColor: '#faf172'}} 
+                className=' text-center p-2 mb-3 rounded-3'
+            >{errMsg}</div>}
+
+            { loading ? (
+                <div className='d-grid gap-2'>
+                    <button class="btn btn-primary py-3" type="button" disabled>
+                        <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                        Loading...
+                    </button>
+                </div>
+            ): (
+                <div className='d-grid gap-2'>
+                    <button className='btn btn-primary  py-3'
+                    type='submit'
+                    >Sign Up</button>
+                </div>
+
+            )}
            
         </form>
     </div>
